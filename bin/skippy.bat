ab5dbe91ef5b6baebfbd9bb6df569b5229eba39e
Thomas Thomassen <thomas@thomthom.net>
2016-08-16 01:15:13 +0200

    Forward arguments in skippy.bat.

--- bin/skippy.bat
+++ bin/skippy.bat
@@ -1,5 +1,3 @@
 @echo off
 REM TODO: Avoid the need for %SKIPPY&
-REM TODO: Avoid the need for multiple hard coded arguments.
-REM ruby %SKIPPY%\..\lib\boot.rb %1 %2 %3 %4 %5 %6 %7 %8 %9
-ruby %SKIPPY%\..\thor\boot.rb %1 %2 %3 %4 %5 %6 %7 %8 %9
+ruby %SKIPPY%\..\thor\boot.rb %*

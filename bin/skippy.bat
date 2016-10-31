2f98d09a0a774bca1b645daed5866add7c081e7f
Thomas Thomassen <thomas@thomthom.net>
2016-10-31 23:33:38 +0100

    Avoid the need for ENV variable for Skippy's path.

--- bin/skippy.bat
+++ bin/skippy.bat
@@ -1,3 +1,6 @@
 @echo off
-REM TODO: Avoid the need for %SKIPPY&
-ruby %SKIPPY%\..\thor\boot.rb %*
+REM %~dp0 returns the path for this batch file.
+REM http://stackoverflow.com/a/659672/486990
+REM
+REM %* forwards all command arguments to Ruby.
+ruby %~dp0\..\thor\boot.rb %*

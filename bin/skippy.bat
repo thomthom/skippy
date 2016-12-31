937c8d1be1826c669c55f85e3854d45f92fd70a5
Thomas Thomassen <thomas@thomthom.net>
2016-12-31 16:06:51 +0100

    Remove obsolete skippy.bat

--- bin/skippy.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM %~dp0 returns the path for this batch file.
-REM http://stackoverflow.com/a/659672/486990
-REM
-REM %* forwards all command arguments to Ruby.
-ruby %~dp0\..\thor\boot.rb %*

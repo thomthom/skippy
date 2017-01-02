56896e54492659145b3bea65e00f29b59c0a0610
Thomas Thomassen <thomas@thomthom.net>
2017-01-02 01:30:07 +0100

    Add debug helper for launching Skippy from source.

---
+++ debug/skippy.bat
@@ -0,0 +1,7 @@
+@echo off
+REM This is used to be able to run Skippy directly from source.
+REM %~dp0 returns the path for this batch file.
+REM http://stackoverflow.com/a/659672/486990
+REM
+REM %* forwards all command arguments to Ruby.
+ruby %~dp0../bin/skippy %*

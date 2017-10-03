db2b98b30ca3103f9f22ecc7a5d0595da26cf3b4
Thomas Thomassen <thomas@thomthom.net>
2017-10-03 21:02:40 +0200

    Update description for debug/skippy.bat.

--- debug/skippy.bat
+++ debug/skippy.bat
@@ -1,5 +1,7 @@
 @echo off
 REM This is used to be able to run Skippy directly from source.
+REM Add its location to the system PATH.
+REM
 REM %~dp0 returns the path for this batch file.
 REM http://stackoverflow.com/a/659672/486990
 REM

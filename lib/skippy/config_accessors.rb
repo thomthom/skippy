55ec7bcecb30c60c9413c5efce231f22eb0f24e6
Thomas Thomassen <thomas@thomthom.net>
2017-12-18 19:43:30 +0100

    Add default param for ConfigAccessor.

--- lib/skippy/config_accessors.rb
+++ lib/skippy/config_accessors.rb
@@ -5,18 +5,18 @@ module Skippy::ConfigAccessors
 
   private
 
-  def config_attr(*symbols, key: nil, type: nil)
-    config_attr_reader(*symbols, key: key, type: type)
+  def config_attr(*symbols, key: nil, default: nil, type: nil)
+    config_attr_reader(*symbols, key: key, type: type, default: default)
     config_attr_writer(*symbols, key: key, type: type)
     nil
   end
 
-  def config_attr_reader(*symbols, key: nil, type: nil)
+  def config_attr_reader(*symbols, key: nil, default: nil, type: nil)
     class_eval do
       symbols.each { |symbol|
         raise TypeError unless symbol.is_a?(Symbol)
         define_method(symbol) do
-          value = @config.get(key || symbol)
+          value = @config.get(key || symbol, default)
           value = type.new(value) if type && !value.is_a?(type)
           value
         end
